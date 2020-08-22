863531d0ba4df8ba681abcbfe8a5e06ddff14ac6
Natasha Seeto <tashseeto@gmail.com>
2020-08-22 12:44:22 +1000

    django_ part 1_ complete add form and image. image set as default

---
+++ she_codes_news/news/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.forms import ModelForm
+from .models import NewsStory
+
+class StoryForm(ModelForm):
+    class Meta:
+        model = NewsStory
+        fields = ['title', 'author', 'pub_date', 'content', 'image_source']
+        widgets = {
+            'pub_date': forms.DateInput(format=('%m/%d/%Y'),
+    attrs={'class':'form-control', 'placeholder':'Select a date', 'type':'date'}),
+        }
--- she_codes_news/news/models.py
+++ she_codes_news/news/models.py
@@ -6,3 +6,4 @@ class NewsStory(models.Model):
     author = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
     content = models.TextField()
+    image_source = models.CharField(max_length=200, default="https://picsum.photos/600")
