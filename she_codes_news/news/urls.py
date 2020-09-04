23b8b0885c58375779d9873a306273313b570efe
Natasha Seeto <tashseeto@gmail.com>
2020-09-04 21:05:21 +1000

    need to revert

--- she_codes_news/news/urls.py
+++ she_codes_news/news/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.StoryView.as_view(), name='story'),
     path('add-story/', views.AddStoryView.as_view(), name='newStory'),
-    # path('author/<int:pk>/', views.AuthorView.as_view(), name='author-detail'),
+#     path('user:author/<int:pk>/', views.AuthorView.as_view(), name='author-detail'),
 ]
